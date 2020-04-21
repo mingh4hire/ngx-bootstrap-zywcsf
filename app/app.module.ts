2888787341988e4f6eb36e317d6d2f0a5470caf7
mingh4hire <mingh4hire@gmail.com>
2020-04-21 16:10:25 -0500

    1 file modified, 3 added

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 import { AlertModule } from 'ngx-bootstrap';
+import { AaComponent } from './aa/aa.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AlertModule.forRoot() ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, AaComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
