f1e02524fc713009c7d20048449efd5c52cc8e11
mingh4hire <mingh4hire@gmail.com>
2020-04-22 15:41:27 -0500

    2 files modified

--- app/aa/aa.component.ts
+++ app/aa/aa.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-aa',
-  templateUrl: './aa.component.html',
+  template: 'asdfsafd',
   styleUrls: ['./aa.component.css']
 })
 export class AaComponent implements OnInit {
